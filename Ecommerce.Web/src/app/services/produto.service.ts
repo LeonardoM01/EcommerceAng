9fa2d53a1c288fcbad7eaaeab6ecfca568d9cb49
Leonardo Miguel <leonardomiguelvs84@gmail.com>
2026-02-23 02:06:53 -0300

    Services

---
+++ Ecommerce.Web/src/app/services/produto.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Produto } from '../models/models';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProdutoService {
+    private apiUrl = 'https://localhost:7091/api/produtos';
+
+    constructor(private http: HttpClient) { }
+
+    getProdutos(): Observable<Produto[]> {
+        return this.http.get<Produto[]>(this.apiUrl);
+    }
+
+    getProduto(id: number): Observable<Produto> {
+        return this.http.get<Produto>(${this.apiUrl}/${id});
+    }
+
+    createProduto(produto: Partial<Produto>): Observable<Produto> {
+        return this.http.post<Produto>(this.apiUrl, produto);
+    }
+}
