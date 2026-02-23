2dba2c8fc2ecee8870c89b4ab948e0af776c2be7
Leonardo Miguel <leonardomiguelvs84@gmail.com>
2026-02-23 03:00:35 -0300

    Adição de Região pt-br

--- Ecommerce.Web/src/app/app.config.ts
+++ Ecommerce.Web/src/app/app.config.ts
@@ -2,8 +2,9 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { LOCALE_ID } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), { provide: LOCALE_ID, useValue: 'pt-BR' }]
 };
