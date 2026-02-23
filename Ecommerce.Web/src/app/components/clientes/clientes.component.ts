1c9cdeb6ebc3489a9610ae65a9135b9e9c178ab8
Leonardo Miguel <leonardomiguelvs84@gmail.com>
2026-02-23 02:18:04 -0300

    Clientes

---
+++ Ecommerce.Web/src/app/components/clientes/clientes.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ClienteService } from '../../services/cliente.service';
+import { Cliente } from '../../models/models';
+
+@Component({
+    selector: 'app-clientes',
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: './clientes.component.html',
+    styleUrls: ['./clientes.component.css']
+})
+export class ClientesComponent implements OnInit {
+    clientes: Cliente[] = [];
+    novoCliente: Partial<Cliente> = { nome: '', cpf: '' };
+
+    constructor(private clienteService: ClienteService) { }
+
+    ngOnInit() {
+        this.carregarClientes();
+    }
+
+    carregarClientes() {
+        this.clienteService.getClientes().subscribe(clientes => {
+            this.clientes = clientes;
+        });
+    }
+
+    salvarCliente() {
+        if (!this.novoCliente.nome || !this.novoCliente.cpf) return;
+        this.clienteService.createCliente(this.novoCliente).subscribe(cliente => {
+            this.clientes.push(cliente);
+            this.novoCliente = { nome: '', cpf: '' };
+        });
+    }
+}
