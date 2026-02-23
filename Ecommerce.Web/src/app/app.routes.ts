4619c0716e68619b264bba894d298108ef8045d3
Leonardo Miguel <leonardomiguelvs84@gmail.com>
2026-02-23 02:17:49 -0300

    Rotas

--- Ecommerce.Web/src/app/app.routes.ts
+++ Ecommerce.Web/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'clientes', loadComponent: () => import('./components/clientes/clientes.component').then(c => c.ClientesComponent) },
+    { path: 'produtos', loadComponent: () => import('./components/produtos/produtos.component').then(c => c.ProdutosComponent) },
+    { path: 'pedidos', loadComponent: () => import('./components/pedidos/pedidos.component').then(c => c.PedidosComponent) },
+    { path: '', redirectTo: '/pedidos', pathMatch: 'full' }
+];
